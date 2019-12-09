a7dec780eb88afb0c05906e60559c29e365f3047
Adarsh Suresh <adarshs3@illinois.edu>
2019-12-08 22:38:55 -0600

    from gallery works on emulator on Adarsh's computer

--- app/src/main/java/com/example/cs125finalproject_wordsearchsolver/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject_wordsearchsolver/MainActivity.java
@@ -2,38 +2,98 @@ package com.example.cs125finalproject_wordsearchsolver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     Button openCamera;
     public static final int REQUEST_IMAGE_CAPTURE = 9;
+    private static final int GALLERY_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pickFromGallery();
+//        Bitmap bmp = BitmapFactory.decodeStream(getApplicationContext().getResources().openRawResource(R.raw.ocrtestwordsearch2));
+//        MyTessOCR mTessOCR;
+//        mTessOCR = new MyTessOCR(MainActivity.this);
+//        String temp = mTessOCR.getOCRResult(bmp);
+//        Log.d("text", temp);
+//        openCamera = findViewById(R.id.openCamera);
+//        openCamera.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (openCamera.getText().toString().equals("Continue")) {
+//                    Intent enterWords = new Intent(MainActivity.this, EnterTheWords.class);
+//                    startActivity(enterWords);
+//                    finish();
+//                } else {
+//                    Intent takeImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    if (takeImage.resolveActivity(getPackageManager()) != null){
+//                        startActivityForResult(takeImage, REQUEST_IMAGE_CAPTURE);
+//                        openCamera.setText("Continue");
+//                    }
+//                }
+//            }
+//        });
+    }
 
-        openCamera = findViewById(R.id.openCamera);
-        openCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (openCamera.getText().toString().equals("Continue")) {
-                    Intent enterWords = new Intent(MainActivity.this, EnterTheWords.class);
-                    startActivity(enterWords);
-                    finish();
-                } else {
-                    Intent takeImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    if (takeImage.resolveActivity(getPackageManager()) != null){
-                        startActivityForResult(takeImage, REQUEST_IMAGE_CAPTURE);
-                        openCamera.setText("Continue");
-                    }
-                }
+    private void pickFromGallery() {
+        //Create an Intent with action as ACTION_PICK
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.setType("image/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        // Launching the Intent
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST_CODE:
+                    //data.getData return the content URI for the selected Image
+                    Log.d("message", "GALLERY_REQUEST_CODE");
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+                    //Get the column index of MediaStore.Images.Media.DATA
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    //Gets the String value in the column
+                    String imgDecodableString = cursor.getString(columnIndex);
+                    cursor.close();
+                    // Set the Image in ImageView after decoding the String
+                    Bitmap bmp = BitmapFactory.decodeFile(imgDecodableString);
+                    Log.d("abs path", imgDecodableString);
+                    MyTessOCR mTessOCR;
+                    mTessOCR = new MyTessOCR(MainActivity.this);
+
+                    String temp = mTessOCR.getOCRResult(bmp);
+                    Log.d("debug", "text produced");
+                    Log.d("text", temp);
+                    break;
+
             }
-        });
     }
 }
---
+++ app/src/main/java/com/example/cs125finalproject_wordsearchsolver/MyTessOCR.java
@@ -0,0 +1,77 @@
+package com.example.cs125finalproject_wordsearchsolver;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.util.Log;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+// CREDIT: API implementation based off https://www.thecodecity.com/2016/09/creating-ocr-android-app-using-tesseract.html
+
+public class MyTessOCR {
+    private String datapath;
+    private TessBaseAPI mTess;    Context context;
+    public MyTessOCR(Context context) {
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        datapath = Environment.getExternalStorageDirectory().toString() + "/ocrctz/";
+        System.out.println(datapath);
+        File dir = new File(datapath + "/tessdata/");
+        File file = new File(datapath + "/tessdata/" + "eng.traineddata");
+
+        if (!file.exists()) {
+            Log.d("mylog", "in file doesn't exist");
+            dir.mkdirs();
+            copyFile(context);
+        }
+        Log.d("debug", "API initialization");
+        mTess = new TessBaseAPI();
+        String language = "eng";
+        Log.d("debug", "before init");
+        mTess.init(datapath, language);
+        Log.d("debug", "after init");
+        //Auto only
+        mTess.setPageSegMode(TessBaseAPI.PageSegMode.PSM_AUTO_ONLY);
+    }
+
+    public void stopRecognition() {
+        mTess.stop();
+    }
+
+    public String getOCRResult(Bitmap bitmap) {
+        Log.d("debug", "before set image");
+        mTess.setImage(bitmap);
+        Log.d("debug", "after set image");
+        String result = mTess.getUTF8Text();
+        Log.d("debug", "after getUTF8Text");
+        return result;
+    }
+
+    public void onDestroy() {
+        if (mTess != null)
+            mTess.end();
+    }
+
+    private void copyFile(Context context) {
+        AssetManager assetManager = context.getAssets();
+        try {
+            Log.d("debug", "copyFile");
+            InputStream in = assetManager.open("eng.traineddata");
+            OutputStream out = new FileOutputStream(datapath + "/tessdata/" + "eng.traineddata");                    byte[] buffer = new byte[1024];
+            int read = in.read(buffer);
+            while (read != -1) {
+                out.write(buffer, 0, read);
+                read = in.read(buffer);
+            }
+        } catch (Exception e) {
+            Log.d("mylog", "couldn't copy with the following error : "+e.toString());
+        }
+    }
+}
